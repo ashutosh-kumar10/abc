d5e63d406e893049f5c40697c2baef5767f9793c
Office <Office@OFFICE-PC>
2018-06-10 15:04:07 +0530

    Tesr

--- NileshVB/frmOne.vb
+++ NileshVB/frmOne.vb
@@ -1,5 +1,6 @@
 ﻿Public Class frmOne
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MessageBox.Show("Sample")
+        MessageBox.Show("Hi")
     End Sub
 End Class
