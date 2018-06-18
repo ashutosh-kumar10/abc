4e2f2e0c61fc6406b290d67b9c04370d67e3ad9a
Deepak Sharma <ds9124965@gmail.com>
2018-06-18 19:07:51 +0530

    adding form

--- NileshVB/Deepak.Designer.vb
+++ NileshVB/Deepak.Designer.vb
@@ -30,7 +30,7 @@ Partial Class Deepak
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.Coral
-        Me.Label1.Location = New System.Drawing.Point(83, 32)
+        Me.Label1.Location = New System.Drawing.Point(72, 32)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(527, 39)
         Me.Label1.TabIndex = 0
