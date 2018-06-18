862a3270644fb3b73a2a2f0898dd2eb5db3ab8e7
Office <Office@OFFICE-PC>
2018-06-18 19:03:57 +0530

    Add a form FrmSum

---
+++ NileshVB/frmSum.vb
@@ -0,0 +1,9 @@
+﻿Public Class frmSum
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a, b, s As Double
+        a = Convert.ToDouble(TextBox1.Text)
+        b = Convert.ToDouble(TextBox2.Text)
+        s = a + b
+        Label1.Text = "Sum " + s
+    End Sub
+End Class
