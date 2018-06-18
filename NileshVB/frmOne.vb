a54031f399baa8da49c152a40e9c745832ea24b5
Deepak Sharma <ds9124965@gmail.com>
2018-06-18 18:56:28 +0530

    Deepak

--- NileshVB/frmOne.vb
+++ NileshVB/frmOne.vb
@@ -5,5 +5,6 @@
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         MessageBox.Show("this is test")
+        MessageBox.Show("this is test")
     End Sub
 End Class
